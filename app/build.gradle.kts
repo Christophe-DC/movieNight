459f81b23a57320dc07a115395d28ae8f60dff7e
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-17 18:36:45 +0200

    feat(login): remove AccountSelectionActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    kotlin("kapt")
     id("dagger.hilt.android.plugin")
+    kotlin("kapt")
 }
 
 android {
@@ -14,10 +14,41 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+    val composeVersion: String by rootProject.extra
     val daggerVersion: String by rootProject.extra
+
+    //Compose
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.runtime:runtime:$composeVersion")
+    implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.24.7-alpha")
+    implementation("com.google.accompanist:accompanist-coil:0.12.0")
 
     // Dagger
     implementation("com.google.dagger:hilt-android:$daggerVersion")
