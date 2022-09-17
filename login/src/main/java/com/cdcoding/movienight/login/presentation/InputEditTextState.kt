2ccc32b4b447d2cf03844219f1c908602dede798
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-17 18:24:00 +0200

    feat(InputEditTextState): create InputEditTextState class

---
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/InputEditTextState.kt
@@ -0,0 +1,7 @@
+package com.cdcoding.movienight.login.presentation
+
+data class InputEditTextState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
