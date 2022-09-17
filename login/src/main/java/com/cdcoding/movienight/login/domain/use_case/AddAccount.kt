4f253d295b19bd1dde52899f594ec341d9c508ad
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-17 18:26:15 +0200

    feat(AddAccount): add delay for simulate latency

--- login/src/main/java/com/cdcoding/movienight/login/domain/use_case/AddAccount.kt
+++ login/src/main/java/com/cdcoding/movienight/login/domain/use_case/AddAccount.kt
@@ -3,6 +3,7 @@ package com.cdcoding.movienight.login.domain.use_case
 import com.cdcoding.movienight.login.domain.model.Account
 import com.cdcoding.movienight.login.domain.model.InvalidAccountException
 import com.cdcoding.movienight.login.domain.repository.AccountRepository
+import kotlinx.coroutines.delay
 
 class AddAccount(
     private val repository: AccountRepository
@@ -10,15 +11,16 @@ class AddAccount(
 
     @Throws(InvalidAccountException::class)
     suspend operator fun invoke(account: Account) {
-        if(account.firstName.isBlank()) {
+        if (account.firstName.isBlank()) {
             throw InvalidAccountException("The first name of the account can't be empty")
         }
-        if(account.lastName.isBlank()) {
+        if (account.lastName.isBlank()) {
             throw InvalidAccountException("The last name of the account can't be empty")
         }
-        if(account.pseudo.isBlank()) {
+        if (account.pseudo.isBlank()) {
             throw InvalidAccountException("The pseudo of the account can't be empty")
         }
+        delay(1000)
         repository.insertAccount(account)
     }
 }
