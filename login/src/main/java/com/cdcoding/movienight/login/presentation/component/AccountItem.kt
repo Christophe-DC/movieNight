c4cc77d1e165e8daba38e38a74677ff4c8534e96
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-19 17:15:30 +0200

    feat(AccoutItem): add params for loading account

--- login/src/main/java/com/cdcoding/movienight/login/presentation/component/AccountItem.kt
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/component/AccountItem.kt
@@ -1,14 +1,13 @@
 package com.cdcoding.movienight.login.presentation.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -17,26 +16,48 @@ import com.cdcoding.movienight.database.data.model.Account
 
 
 @Composable
-fun AccountItem(account: Account) {
-    Column {
+fun AccountItem(
+    account: Account,
+    isEnabled: Boolean = true,
+    isLoading: Boolean = false,
+    onAccountItemClick: (account: Account) -> Unit
+) {
+    Column(
+        modifier = Modifier.alpha(
+            if (!isEnabled && !isLoading) 0.4f
+            else 1f
+        )
+    ) {
         Row(
             modifier = Modifier
                 .width(80.dp)
                 .height(80.dp)
-                .clip(shape = CircleShape)
-                .background(MaterialTheme.colors.secondary),
+                .clip(shape = CircleShape),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        )
-        {
-            Text(
-                text = "${account.firstName.first().uppercase()}${
-                    account.lastName.first().uppercase()
-                }",
-                style = typography.button.copy(fontSize = 30.sp),
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colors.onSecondary
+        ) {
+            Button(
+                modifier = Modifier
+                    .fillMaxSize(),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.secondary
+                ),
+                onClick = { if (isEnabled) onAccountItemClick(account) }
             )
+            {
+                if (isLoading) {
+                    CircularProgressIndicator()
+                } else {
+                    Text(
+                        text = "${account.firstName.first().uppercase()}${
+                            account.lastName.first().uppercase()
+                        }",
+                        style = typography.button.copy(fontSize = 30.sp),
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colors.onSecondary
+                    )
+                }
+            }
         }
         Row(
             modifier = Modifier
