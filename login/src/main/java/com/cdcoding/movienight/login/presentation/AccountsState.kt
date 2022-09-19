19f91598134d56add3a4632dea0cbab0446ff916
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-19 17:17:21 +0200

    feat(AccountsState): add accountLoading param

--- login/src/main/java/com/cdcoding/movienight/login/presentation/AccountsState.kt
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/AccountsState.kt
@@ -1,9 +1,10 @@
 package com.cdcoding.movienight.login.presentation
 
-import com.cdcoding.movienight.login.domain.model.Account
+import com.cdcoding.movienight.database.data.model.Account
 
 data class AccountsState(
     val accounts: List<Account> = emptyList(),
     val isNewAccountModalVisible: Boolean = false,
     val isNewAccountCreating: Boolean = false,
+    val accountLoading: Account? = null,
 )
