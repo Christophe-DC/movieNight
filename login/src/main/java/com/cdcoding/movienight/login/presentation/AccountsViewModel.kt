1cee534fb84b6ca012cdd31d9280ac9329a52159
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-19 17:19:49 +0200

    feat(AccountsViewModel): implement movies worker

--- login/src/main/java/com/cdcoding/movienight/login/presentation/AccountsViewModel.kt
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/AccountsViewModel.kt
@@ -1,21 +1,24 @@
 package com.cdcoding.movienight.login.presentation
 
+import android.app.Application
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
-import com.cdcoding.movienight.login.domain.model.Account
+import androidx.work.*
 import com.cdcoding.movienight.database.data.model.Account
 import com.cdcoding.movienight.database.data.model.InvalidAccountException
 import com.cdcoding.movienight.login.domain.use_case.AccountUseCases
+import com.cdcoding.movienight.login.worker.GetMoviesWorker
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AccountsViewModel @Inject constructor(
+    application: Application,
     private val accountUseCases: AccountUseCases
 ) : ViewModel() {
 
@@ -31,8 +34,24 @@ class AccountsViewModel @Inject constructor(
     private val _accountPseudo = mutableStateOf(InputEditTextState())
     val accountPseudo: State<InputEditTextState> = _accountPseudo
 
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private var accountLoading: Account? = null
+    private val workManager = WorkManager.getInstance(application)
+    private val moviesWorker = OneTimeWorkRequestBuilder<GetMoviesWorker>()
+        .setConstraints(
+            Constraints.Builder()
+                .setRequiredNetworkType(
+                    NetworkType.CONNECTED
+                )
+                .build()
+        )
+        .build()
+
     init {
         getAccounts()
+        observeMovieWorker()
     }
 
     fun onEvent(event: AccountsEvent) {
@@ -47,7 +66,7 @@ class AccountsViewModel @Inject constructor(
                             Account(
                                 firstName = accountFirstName.value.text,
                                 lastName = accountLastName.value.text,
-                                pseudo =  accountPseudo.value.text
+                                pseudo = accountPseudo.value.text
                             )
                         )
                         _state.value = state.value.copy(
@@ -105,6 +124,9 @@ class AccountsViewModel @Inject constructor(
                             accountPseudo.value.text.isBlank()
                 )
             }
+            is AccountsEvent.OpenAccount -> {
+                fetchMovies(event.account)
+            }
         }
     }
 
@@ -116,9 +138,63 @@ class AccountsViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    private fun initNewAccountFields(){
-        _accountFirstName.value = InputEditTextState( hint = "Gérard")
-        _accountLastName.value = InputEditTextState( hint = "Depardieu")
-        _accountPseudo.value = InputEditTextState( hint = "Gégé")
+    private fun initNewAccountFields() {
+        _accountFirstName.value = InputEditTextState(hint = "Gérard")
+        _accountLastName.value = InputEditTextState(hint = "Depardieu")
+        _accountPseudo.value = InputEditTextState(hint = "Gégé")
+    }
+
+
+    private fun observeMovieWorker() {
+        val workInfo: Flow<WorkInfo> = workManager.getWorkInfoByIdLiveData(moviesWorker.id).asFlow()
+        viewModelScope.launch {
+            workInfo.collect {
+                when (it.state) {
+                    WorkInfo.State.ENQUEUED, WorkInfo.State.RUNNING -> {
+                        _state.value = state.value.copy(
+                            accountLoading = accountLoading
+                        )
+                    }
+                    WorkInfo.State.SUCCEEDED -> {
+                        _state.value = state.value.copy(
+                            accountLoading = null
+                        )
+                        _eventFlow.emit(
+                            UiEvent.MoviesFetched(accountId = accountLoading?.id)
+                        )
+                    }
+                    WorkInfo.State.FAILED -> {
+                        _state.value = state.value.copy(
+                            accountLoading = null
+                        )
+                        _eventFlow.emit(
+                            UiEvent.ShowToast(
+                                message = "Unknown network error"
+                            )
+                        )
+                    }
+                    WorkInfo.State.CANCELLED -> {
+                        _state.value = state.value.copy(
+                            accountLoading = null
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchMovies(account: Account) {
+        accountLoading = account
+        workManager.beginUniqueWork(
+            "GET_MOVIES",
+            ExistingWorkPolicy.REPLACE,
+            moviesWorker
+        ).enqueue()
+    }
+
+
+    sealed class UiEvent {
+        data class ShowToast(val message: String) : UiEvent()
+        data class MoviesFetched(val accountId: Int?) : UiEvent()
     }
 }
