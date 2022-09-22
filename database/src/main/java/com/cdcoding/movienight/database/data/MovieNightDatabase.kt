12c0044b8f767906126d39730ff7266e2e6b5cbd
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-22 10:59:36 +0200

    feat(MovieNightDatabase): add MovieDetail entity

--- database/src/main/java/com/cdcoding/movienight/database/data/MovieNightDatabase.kt
+++ database/src/main/java/com/cdcoding/movienight/database/data/MovieNightDatabase.kt
@@ -2,19 +2,25 @@ package com.cdcoding.movienight.database.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.cdcoding.movienight.database.data.model.Account
+import com.cdcoding.movienight.database.data.model.ListConverter
 import com.cdcoding.movienight.database.data.model.Movie
+import com.cdcoding.movienight.database.data.model.MovieDetail
 
 @Database(
     entities = [
         Account::class,
-        Movie::class],
+        Movie::class,
+        MovieDetail::class],
     version = 1
 )
+@TypeConverters(ListConverter::class)
 internal abstract class MovieNightDatabase : RoomDatabase() {
 
     abstract val accountDao: AccountDao
     abstract val movieDao: MovieDao
+    abstract val movieDetailDao: MovieDetailDao
 
     companion object {
         const val DATABASE_NAME = "movie_night_db"
