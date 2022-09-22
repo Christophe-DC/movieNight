46f26162665fb69cbfe85e3752881260d13e8726
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-22 10:55:49 +0200

    feat(Color): add DarkGrey color

--- app/src/main/java/com/cdcoding/movienight/ui/theme/Color.kt
+++ app/src/main/java/com/cdcoding/movienight/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.cdcoding.movienight.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 val NightBlue = Color(0xFF2E2B3D)
 val DarkNightBlue = Color(0xFF040722)
 val LightBlue = Color(0xFFE0DCF5)
+val DarkGrey = Color(0xFF2A2A2A)
