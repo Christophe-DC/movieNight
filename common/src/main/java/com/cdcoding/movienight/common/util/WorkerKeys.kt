38ab2d60eb02d3c07e717ea6b7c64d7f2958ef9f
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-22 10:49:25 +0200

    feat(WorkerKeys): add MOVIE_DETAIL_ID constant

---
+++ common/src/main/java/com/cdcoding/movienight/common/util/WorkerKeys.kt
@@ -0,0 +1,7 @@
+package com.cdcoding.movienight.common.util
+
+object WorkerKeys {
+    const val ERROR_MSG = "errorMsg"
+    const val MOVIES = "movies"
+    const val MOVIE_DETAIL_ID = "movieDetailId"
+}
--- login/src/main/java/com/cdcoding/movienight/login/worker/WorkerKeys.kt
+++
@@ -1,6 +0,0 @@
-package com.cdcoding.movienight.login.worker
-
-object WorkerKeys {
-    const val ERROR_MSG = "errorMsg"
-    const val MOVIES = "movies"
-}
