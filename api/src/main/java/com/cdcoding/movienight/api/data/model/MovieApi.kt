9c85bcfdd60dea6e315d12b2c5519ad95c684d7c
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-22 10:54:42 +0200

    feat(api): implement fetchMovieById expose method

--- api/src/main/java/com/cdcoding/movienight/api/data/model/MovieApi.kt
+++ api/src/main/java/com/cdcoding/movienight/api/data/model/MovieApi.kt
@@ -1,9 +1,12 @@
 package com.cdcoding.movienight.api.data.model
 
+import androidx.compose.ui.text.intl.Locale
 import com.cdcoding.movienight.api.BuildConfig
+import com.cdcoding.movienight.api.data.model.dto.MovieDetailDto
 import com.cdcoding.movienight.api.data.model.dto.MoviesDto
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 internal interface MovieApi {
 
@@ -12,4 +15,11 @@ internal interface MovieApi {
         @Path("media_type") mediaType: String = "movie",
         @Path("time_window") timeWindow: String = "day"
     ): MoviesDto
+
+    @GET("movie/{movie_id}?api_key=${BuildConfig.API_KEY}")
+    suspend fun getMovieDetail(
+        @Path("movie_id") movieId: Int,
+        @Query("language") language: String = "${Locale.current.language}-${Locale.current.region}",
+        @Query("append_to_response") responseType: String = "videos"
+    ): MovieDetailDto
 }
--- api/src/main/java/com/cdcoding/movienight/api/data/model/repository/MovieRemoteRepositoryImpl.kt
+++ api/src/main/java/com/cdcoding/movienight/api/data/model/repository/MovieRemoteRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.cdcoding.movienight.api.data.model.repository
 
 import com.cdcoding.movienight.api.data.model.MovieApi
+import com.cdcoding.movienight.api.data.model.dto.toMovieDetail
 import com.cdcoding.movienight.api.data.model.dto.toMovies
 import com.cdcoding.movienight.api.domain.repository.MovieRemoteRepository
+import com.cdcoding.movienight.database.data.model.MovieDetail
 import com.cdcoding.movienight.database.data.model.Movies
 
 internal class MovieRemoteRepositoryImpl(
@@ -11,6 +13,10 @@ internal class MovieRemoteRepositoryImpl(
 
     override suspend fun fetchMovies(): Movies {
         return movieApi.getMovies().toMovies()
+    }
+
+    override suspend fun fetchMovieById(id: Int): MovieDetail {
+        return movieApi.getMovieDetail(id).toMovieDetail()
     }
 
 }
