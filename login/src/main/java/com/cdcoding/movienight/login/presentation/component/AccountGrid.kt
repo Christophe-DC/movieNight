20978c2079df1e04fb8fadb392cb5d3656b678ed
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-23 16:00:55 +0200

    fix(AccountGrid): run account grid animation once time

--- login/src/main/java/com/cdcoding/movienight/login/presentation/component/AccountGrid.kt
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/component/AccountGrid.kt
@@ -4,9 +4,8 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -29,30 +28,43 @@ fun AccountGrid(
         modifier = modifier,
         contentAlignment = Alignment.TopCenter
     ) {
+
+        var runAnimation by rememberSaveable { mutableStateOf(true) }
         val offsetY = remember {
-            Animatable(250f)
+            Animatable(
+                if (runAnimation) 250f
+                else 50f
+            )
         }
         val alpha = remember {
-            Animatable(0f)
+            Animatable(
+                if (runAnimation) 0f
+                else 1f
+            )
         }
-        LaunchedEffect(key1 = true) {
+        LaunchedEffect(runAnimation) {
             launch {
-                offsetY.animateTo(
-                    targetValue = 50f,
-                    animationSpec = tween(
-                        durationMillis = durationMillis,
-                        delayMillis = delayMillis
+                if (runAnimation) {
+                    offsetY.animateTo(
+                        targetValue = 50f,
+                        animationSpec = tween(
+                            durationMillis = durationMillis,
+                            delayMillis = delayMillis
+                        )
                     )
-                )
+                }
             }
             launch {
-                alpha.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(
-                        durationMillis = durationMillis,
-                        delayMillis = delayMillis
+                if (runAnimation) {
+                    alpha.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(
+                            durationMillis = durationMillis,
+                            delayMillis = delayMillis
+                        )
                     )
-                )
+                    runAnimation = false
+                }
             }
         }
 
