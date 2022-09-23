a79602b8d379154de6e9e8cbe49da5e5a7f43b97
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-23 16:00:23 +0200

    fix(AppTitle): run title animation once time

--- login/src/main/java/com/cdcoding/movienight/login/presentation/component/AppTitle.kt
+++ login/src/main/java/com/cdcoding/movienight/login/presentation/component/AppTitle.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -24,29 +25,44 @@ fun AppTitle(
     Box(
         modifier = modifier
     ) {
-        val verticalBias = remember { Animatable(-0.25f) }
+
+        var runAnimation by rememberSaveable { mutableStateOf(true) }
+        val verticalBias = remember {
+            Animatable(
+                if (runAnimation) -0.25f
+                else -1f
+            )
+        }
         val alignment by derivedStateOf { BiasAlignment.Vertical(verticalBias.value) }
         val scale = remember {
-            Animatable(2f)
+            Animatable(
+                if (runAnimation) 2f
+                else 1f
+            )
         }
-        LaunchedEffect(key1 = true) {
+        LaunchedEffect(runAnimation) {
             launch {
-                verticalBias.animateTo(
-                    targetValue = -1f,
-                    animationSpec = tween(
-                        durationMillis = durationMillis,
-                        delayMillis = delayMillis
+                if (runAnimation) {
+                    verticalBias.animateTo(
+                        targetValue = -1f,
+                        animationSpec = tween(
+                            durationMillis = durationMillis,
+                            delayMillis = delayMillis
+                        )
                     )
-                )
+                }
             }
             launch {
-                scale.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(
-                        durationMillis = durationMillis,
-                        delayMillis = delayMillis
+                if (runAnimation) {
+                    scale.animateTo(
+                        targetValue = 1f,
+                        animationSpec = tween(
+                            durationMillis = durationMillis,
+                            delayMillis = delayMillis
+                        )
                     )
-                )
+                    runAnimation = false
+                }
             }
         }
         Row(
