56b3a7e176bc6fd5567904899ecb2f16a7c95f2d
Christophe <da.costa.christophe.mail@gmail.com>
2022-09-23 16:27:57 +0200

    feat(Screen): add MovieDetailScreen object

--- common/src/main/java/com/cdcoding/movienight/common/util/Screen.kt
+++ common/src/main/java/com/cdcoding/movienight/common/util/Screen.kt
@@ -3,4 +3,5 @@ package com.cdcoding.movienight.common.util
 sealed class Screen(val route: String) {
     object AccountsScreen: Screen("accounts_screen")
     object MoviesScreen: Screen("movies_screen")
+    object MovieDetailScreen: Screen("movie_detail_screen")
 }
